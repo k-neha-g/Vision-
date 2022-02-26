4f07273badfc0eb09dbd43200a9dd6d0f9e02e71
Neha Gupta <89221565+k-neha-g@users.noreply.github.com>
2022-02-27 00:05:21 +0530

    Add files via upload

---
+++ vision-main/script.js
@@ -0,0 +1,35 @@
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log("sidebarBtn");
+sidebarBtn.addEventListener("click", ()=>{
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if(isMobile){
+  if(document.getElementsByClassName('sidebar')[0].style.display == '' || document.getElementsByClassName('sidebar')[0].style.display == 'none'){
+    document.getElementsByClassName('sidebar')[0].style.display ="block";
+    document.getElementsByClassName('home-section')[0].style.position ="relative";
+    console.log("if...");
+    sidebar.classList.toggle("close");
+
+  }
+  else{
+    console.log("else...");
+    document.getElementsByClassName('sidebar')[0].style.display = 'none';
+    document.getElementsByClassName('home-section')[0].style.position ="initial";
+    document.getElementsByClassName('home-section')[0].style.width="100%";
+    sidebar.classList.toggle("close");
+  }
+}
+else{
+    sidebar.classList.toggle("close");
+}
+});
+
+
